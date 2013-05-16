e3f37b2dc87fdbe67dbfc20f77a27ed5a364a1ea
Guus Beckett <guus@reupload.nl>
2013-05-16 15:16:22 +0200

    Entity and HostileEntity base finished

--- src/model/entities/HostileEntity.java
+++ src/model/entities/HostileEntity.java
@@ -1,6 +1,35 @@
 package model.entities;
 
+import java.awt.geom.Rectangle2D;
+
 public class HostileEntity extends Entity
 {
+	private int reward;
+	protected boolean alive;
+	
+	public HostileEntity(Rectangle2D bounds, int reward)
+	{
+		super(bounds, null);
+		this.reward = reward;
+	}
+	
+	public int getReward()
+	{
+		return reward;
+	}
 
+	public void setReward(int reward)
+	{
+		this.reward = reward;
+	}
+
+	public boolean isAlive()
+	{
+		return alive;
+	}
+
+	public void kill()
+	{
+		alive = false;
+	}
 }
