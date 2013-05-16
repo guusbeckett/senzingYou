06f009b6fdd7dbfcaabc478f9904f0f05987035b
Sven van Heugten <svenvanheugten@home.nl>
2013-05-16 12:53:40 +0200

    Added basic frame code.

--- src/view/SenzingFrame.java
+++ src/view/SenzingFrame.java
@@ -4,7 +4,14 @@ import javax.swing.JFrame;
 
 public class SenzingFrame extends JFrame
 {
-
 	private static final long serialVersionUID = 1L;
 
+	public SenzingFrame()
+	{
+		super("Senzing");
+		setSize(640, 480);
+		setContentPane(new SenzingPanel());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
--- src/view/SenzingPanel.java
+++ src/view/SenzingPanel.java
@@ -4,7 +4,10 @@ import javax.swing.JPanel;
 
 public class SenzingPanel extends JPanel
 {
-
 	private static final long serialVersionUID = 1L;
 
+	public SenzingPanel()
+	{
+		
+	}
 }
