abb03acd69edc6d4f644e6a779c85305194eba77
Jan Willem Raats <jw.raats@gmail.com>
2013-05-16 15:07:28 +0200

    Kinect now sends a RGB image correctly hopefully ;P

--- src/control/CameraController.java
+++ src/control/CameraController.java
@@ -1,5 +1,9 @@
 package control;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+
 import model.CameraData;
 import model.Game;
 
@@ -20,6 +24,10 @@ public class CameraController
 		{
 			OutArg<ScriptNode> scriptNode = new OutArg<ScriptNode>();
 			cameraData = new CameraData(Context.createFromXmlFile("./OpenNIConfig.xml", scriptNode));
+			
+			//RGB image
+			cameraData.setImage(new BufferedImage(cameraData.getImageGenerator().getMetaData().getFullXRes(), cameraData.getImageGenerator().getMetaData().getFullYRes(), BufferedImage.TYPE_INT_RGB));
+			
 			cameraData.getContext().startGeneratingAll();
 		}
 		catch(Exception e)
@@ -27,4 +35,30 @@ public class CameraController
 			
 		}
 	}
+	
+	public void update(){
+		updateRGBCamera();
+		
+	}
+
+	private void updateRGBCamera(){
+		int[] imageRGBArray = new int[cameraData.getImageGenerator().getMetaData().getFullXRes() * cameraData.getImageGenerator().getMetaData().getFullYRes()];
+		
+		int i = 0;
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		ByteBuffer rgbBuffer = cameraData.getImageGenerator().getMetaData().getData().createByteBuffer();
+		for (int x = 0; x < cameraData.getImageGenerator().getMetaData().getFullXRes(); x++) {
+			for (int y = 0; y < cameraData.getImageGenerator().getMetaData().getFullYRes(); y++) {
+				i = y * cameraData.getImageGenerator().getMetaData().getFullXRes() + x;
+				r = rgbBuffer.get(i * 3) & 0xff;
+				g = rgbBuffer.get(i * 3 + 1) & 0xff;
+				b = rgbBuffer.get(i * 3 + 2) & 0xff;
+				imageRGBArray[i] = (r << 16) | (g << 8) | b;
+			}
+		}
+		cameraData.getImage().setRGB(0, 0, cameraData.getImageGenerator().getMetaData().getFullXRes(), cameraData.getImageGenerator().getMetaData().getFullYRes(), imageRGBArray, 0, cameraData.getImageGenerator().getMetaData().getFullXRes());
+	}
+	
 }
--- src/model/CameraData.java
+++ src/model/CameraData.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 import org.OpenNI.Context;
 import org.OpenNI.DepthGenerator;
 import org.OpenNI.GeneralException;
 import org.OpenNI.HandsGenerator;
+import org.OpenNI.ImageGenerator;
 import org.OpenNI.UserGenerator;
 
 public class CameraData
@@ -16,6 +19,10 @@ public class CameraData
 	private DepthGenerator depthGenerator;
 	private UserGenerator userGenerator;
 	private HandsGenerator handsGenerator;
+	private ImageGenerator imageGenerator;
+	
+	private BufferedImage image;
+	
 	private List<Hand> hands;
 	private List<User> users;
 
@@ -27,6 +34,7 @@ public class CameraData
 			this.depthGenerator = DepthGenerator.create(context);
 			this.userGenerator = UserGenerator.create(context);
 			this.handsGenerator = HandsGenerator.create(context);
+			this.imageGenerator = ImageGenerator.create(context);
 		} catch (GeneralException e)
 		{
 			// TODO Auto-generated catch block
@@ -63,4 +71,37 @@ public class CameraData
 	{
 		return users;
 	}
+
+	public ImageGenerator getImageGenerator()
+	{
+		return imageGenerator;
+	}
+
+	public BufferedImage getImage()
+	{
+	int[] imageRGBArray = new int[getImageGenerator().getMetaData().getFullXRes() * getImageGenerator().getMetaData().getFullYRes()];
+		
+		int i = 0;
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		ByteBuffer rgbBuffer = getImageGenerator().getMetaData().getData().createByteBuffer();
+		for (int x = 0; x < getImageGenerator().getMetaData().getFullXRes(); x++) {
+			for (int y = 0; y < getImageGenerator().getMetaData().getFullYRes(); y++) {
+				i = y * getImageGenerator().getMetaData().getFullXRes() + x;
+				r = rgbBuffer.get(i * 3) & 0xff;
+				g = rgbBuffer.get(i * 3 + 1) & 0xff;
+				b = rgbBuffer.get(i * 3 + 2) & 0xff;
+				imageRGBArray[i] = (r << 16) | (g << 8) | b;
+			}
+		}
+		getImage().setRGB(0, 0, getImageGenerator().getMetaData().getFullXRes(), getImageGenerator().getMetaData().getFullYRes(), imageRGBArray, 0, getImageGenerator().getMetaData().getFullXRes());
+		return image;
+	}
+
+	public void setImage(BufferedImage image)
+	{
+		this.image = image;
+	}
+
 }
