fe527525dfbdca67506652511b140309beeef208
Sven van Heugten <svenvanheugten@home.nl>
2013-05-17 11:11:08 +0200

    Fish now moves as a fish.

--- src/model/entities/underwater/Fish.java
+++ src/model/entities/underwater/Fish.java
@@ -1,5 +1,6 @@
 package model.entities.underwater;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import model.CameraData;
@@ -7,15 +8,22 @@ import model.entities.Entity;
 
 public class Fish extends Entity
 {
+	private double baseY;
+	private final static double WIDTH = 60;
+	private final static double HEIGHT = 40;
+	
 	public Fish()
 	{
-		super(new Rectangle2D.Double(0, 0, 60, 40), null);
+		super(new Rectangle2D.Double(-WIDTH, 0, WIDTH, HEIGHT), null);
+		
+		setY((baseY = Math.random() * (CameraData.VIEW_HEIGHT - HEIGHT * 4) + HEIGHT * 2));
+		velocity = new Point2D.Double(Math.random() * 0.2 + 0.01, 0);
 	}
 
 	@Override
 	public void update(double time)
 	{
-		setX(getBounds().getX() + 0.01 * time);
-		setY(Math.sin(getBounds().getX() / CameraData.VIEW_WIDTH * 2 * Math.PI) * 30);
+		setX(getBounds().getX() + velocity.getX() * time);
+		setY(baseY + Math.sin(getBounds().getX() / CameraData.VIEW_WIDTH * 2 * Math.PI) * HEIGHT);
 	}
 }
