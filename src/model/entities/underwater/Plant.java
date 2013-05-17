d1937ab5215cd22c1cb55fe7716e9fe1ec32f4e2
Guus Beckett <guus@reupload.nl>
2013-05-17 11:13:36 +0200

    Added location spawning for plant

--- src/model/entities/underwater/Plant.java
+++ src/model/entities/underwater/Plant.java
@@ -1,18 +1,35 @@
 package model.entities.underwater;
 
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.util.List;
 
 import model.entities.Entity;
 
 public class Plant extends Entity
 {
 
-	public Plant(Rectangle2D bounds, List<BufferedImage> images)
+	private double degrees;
+	
+	AffineTransform identity = new AffineTransform();
+	public Plant()
 	{
-		super(bounds, images);
-		// TODO Auto-generated constructor stub
+		super(new Rectangle2D.Double(0, 0, 0, 0), null);
+		setLocation();
+	}
+	
+	public void setLocation()
+	{
+		setX((Math.random() * (640 - 1) + 1));
+		setY(480);
+	}
+	
+	public void update(double time)
+	{
+		if(time>=180)time-=180;
+		degrees=Math.sin(Math.toRadians(time));
+		degrees*=90;
+		degrees-=45;
+		setRotation(degrees);
 	}
 
 }
