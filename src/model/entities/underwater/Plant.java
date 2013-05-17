8da7037984105f441022b1506e8a05d2ac90f509
Guus Beckett <guus@reupload.nl>
2013-05-17 11:29:10 +0200

    Added rotationpoint and rotation to Plant entity

--- src/model/entities/underwater/Plant.java
+++ src/model/entities/underwater/Plant.java
@@ -1,6 +1,7 @@
 package model.entities.underwater;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import model.entities.Entity;
@@ -8,14 +9,18 @@ import model.entities.Entity;
 public class Plant extends Entity
 {
 
+	private static int with = 30;
+	private static int height = 150;
+
 	private double degrees;
 	
 	AffineTransform identity = new AffineTransform();
 	public Plant()
 	{
-		super(new Rectangle2D.Double(0, 0, 0, 0), null);
+		super(new Rectangle2D.Double(0, 0, with, height), null);
 		setX((Math.random() * (640 - 1) + 1));
 		setY(480);
+		setRotationPoint(new Point2D.Double(with/2,0));
 	}
 	
 	public void update(double time)
