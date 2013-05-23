7816859dd040146fba72a32e49dcab5d79ff5aad
Guus Beckett <guus@reupload.nl>
2013-05-23 15:39:05 +0200

    Added droplet and Rock entities

--- src/model/entities/cave/Droplet.java
+++ src/model/entities/cave/Droplet.java
@@ -1,47 +1,54 @@
 package model.entities.cave;
 
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
+import model.Camera;
 import model.entities.Entity;
 
 public class Droplet extends Entity
 {
+	int xLocation = (int) (Math.random() * (Camera.VIEW_WIDTH - 1) + 1);
+	int yLocation; 
 	public Droplet()
 	{
 		super();
-		// TODO Auto-generated constructor stub
+		position.setLocation(xLocation, 0);	
 	}
 
 	@Override
 	public Point2D getRotationPoint()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new Point2D.Double(getDimensions().getWidth()/2,getDimensions().getHeight()/2);
 	}
 
 	@Override
 	public Dimension2D getDimensions()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new Dimension(30, 30);
 	}
 
 	@Override
 	public List<Image> getImages()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Image> images = new ArrayList<Image>();
+		
+		images.add(Toolkit.getDefaultToolkit().getImage("./images/cave/droplet.png"));
+		
+		return images;
 	}
 
 	@Override
 	public void update(double time)
 	{
-		super.update(time);
-		// TODO Auto-generated method stub
-		
+		super.update(time);	
+		yLocation += (int) (1/2 * 10 * Math.pow(time, 2));
+		position.setLocation(xLocation, yLocation);
 	}
 
 }
