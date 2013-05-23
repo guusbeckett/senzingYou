3a01595718c88566a9463767fcf61fd2e1c9ddf3
imp <johannes.san@gmail.com>
2013-05-23 22:37:39 +0200

    added abstract methods implementation

--- src/control/levels/CaveLevel.java
+++ src/control/levels/CaveLevel.java
@@ -3,6 +3,8 @@ package control.levels;
 import java.awt.Toolkit;
 
 import model.Game;
+import model.entities.Entity;
+import model.entities.HostileEntity;
 import model.entities.cave.Droplet;
 import model.entities.cave.Rock;
 
@@ -19,12 +21,23 @@ public class CaveLevel extends DodgeLevel
 	public void update(double time)
 	{
 		super.update(time);
-		if((int)(Math.random() * time) == 1){
-			getGame().getEntities().add(new Droplet());
-		}
-		if((int)(Math.random() * (time * 0.5)) == 1){
-			getGame().getEntities().add(new Rock(getGame().getCamera().getUsers()));
-		}
-		
+//		if((int)(Math.random() * time) == 1){
+//			getGame().getEntities().add(new Droplet());
+//		}
+//		if((int)(Math.random() * (time * 0.5)) == 1){
+//			getGame().getEntities().add(new Rock(getGame().getCamera().getUsers()));
+//		}
+	}
+
+	@Override
+	public Entity getRandomEntity()
+	{
+		return new Droplet();
+	}
+
+	@Override
+	public HostileEntity getRandomHostileEntity()
+	{
+		return new Rock(getGame().getCamera().getUsers());
 	}
 }
