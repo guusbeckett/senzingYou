4c8ec84edb2a56752e6683595f2c6c5892e4798a
imp <johannes.san@gmail.com>
2013-05-23 14:46:45 +0200

    added all needed classes for the audio analysis! I hope.

---
+++ src/control/Song.java
@@ -0,0 +1,33 @@
+package control;
+
+public class Song
+{
+	public Song(String track)
+	{
+		//TODO: implement constructor
+	}
+	
+	public void play()
+	{
+		//TODO: implement play song
+	}
+	
+	public float getThreshold()
+	{
+		//TODO: implement return
+		//return average threshold since last call
+		return 0f;
+	}
+	
+	public float getThreshold(int time)
+	{
+		//TODO: implement return
+		return 0f;
+	}
+	
+	public int getTime()
+	{
+		//TODO: implement return
+		return 0;
+	}
+}
