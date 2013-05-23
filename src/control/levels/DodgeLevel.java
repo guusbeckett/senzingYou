be681454a765f354db5ea12a971dec2ba291f2f4
imp <johannes.san@gmail.com>
2013-05-23 22:35:20 +0200

    change to the way punch level spawns

--- src/control/levels/DodgeLevel.java
+++ src/control/levels/DodgeLevel.java
@@ -1,6 +1,5 @@
 package control.levels;
 
-import java.awt.geom.Rectangle2D;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,19 +9,27 @@ import model.User;
 import model.entities.Entity;
 import model.entities.HostileEntity;
 
-public class DodgeLevel extends Level
+public abstract class DodgeLevel extends Level
 {
+	private float proportion;
+	private float minThreshold;
+	private int lastSpawnedHostile = 0;
+	private int lastSpawned = 0;
 
 	public DodgeLevel(Game game)
 	{
 		super(game);
-		// TODO Auto-generated constructor stub
+		if(getGame().getSong() != null){
+			minThreshold = getGame().getSong().getMinThreshold();
+			proportion = getGame().getSong().getMaxThreshold() - minThreshold;
+		}
 	}
 	
 	@Override
 	public void update(double time)
 	{
 		super.update(time);
+		spawn(time);
 
 		List<Entity> entities = getGame().getEntities();
 		Iterator<Entity> it = entities.iterator();
@@ -61,4 +68,30 @@ public class DodgeLevel extends Level
 		}
 	}
 
+	public abstract Entity getRandomEntity();
+
+	public abstract HostileEntity getRandomHostileEntity();
+
+	public void spawn(double time)
+	{
+		float current = (getGame().getSong().getThreshold() - minThreshold)
+				/ proportion;
+
+		if (current != 0)
+		{
+			if (lastSpawnedHostile > (100 / (current * current * current)))
+			{
+				getGame().getEntities().add(getRandomHostileEntity());
+				lastSpawnedHostile = 0;
+			} else
+				lastSpawnedHostile += time;
+			
+			if(lastSpawned > 100 / current)
+			{
+				getGame().getEntities().add(getRandomEntity());
+				lastSpawned = 0;
+			} else
+				lastSpawned += time;
+		}
+	}
 }
