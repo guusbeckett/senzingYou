dade517dd4fcc788e0c53c9c5372587212d68528
imp <johannes.san@gmail.com>
2013-05-24 12:31:50 +0200

    moved abstract and spawn methods to Level.java

--- src/control/levels/Level.java
+++ src/control/levels/Level.java
@@ -2,27 +2,69 @@ package control.levels;
 
 import model.Game;
 import model.entities.Entity;
+import model.entities.HostileEntity;
 
-public class Level
+public abstract class Level
 {
+	private float proportion;
+	private float minThreshold;
+	private int lastSpawnedHostile = 0;
+	private int lastSpawned = 0;
 	private Game game;
 
 	public Level(Game game)
 	{
 		super();
 		this.game = game;
+		if (getGame().getSong() != null)
+		{
+			minThreshold = getGame().getSong().getMinThreshold();
+			proportion = getGame().getSong().getMaxThreshold() - minThreshold;
+		}
 	}
-	
+
 	public void update(double time)
 	{
-		for(Entity entity : game.getEntities())
+		if (getGame().getSong() != null)
 		{
-			entity.update(time);
+			float current = (getGame().getSong().getThreshold() - minThreshold)
+					/ proportion;
+			spawn(time, current);
+
+			for (Entity entity : game.getEntities())
+			{
+				entity.update(time * current);
+			}
 		}
 	}
 
 	public Game getGame()
 	{
 		return game;
+	}
+
+	public abstract Entity getRandomEntity();
+
+	public abstract HostileEntity getRandomHostileEntity();
+
+	public void spawn(double time, float current)
+	{
+
+		if (current != 0)
+		{
+			if (lastSpawnedHostile > (100 / (current * current * current)))
+			{
+				getGame().getEntities().add(getRandomHostileEntity());
+				lastSpawnedHostile = 0;
+			} else
+				lastSpawnedHostile += time;
+
+			if (lastSpawned > 100 / current)
+			{
+				getGame().getEntities().add(getRandomEntity());
+				lastSpawned = 0;
+			} else
+				lastSpawned += time;
+		}
 	}
 }
--- src/control/levels/PunchLevel.java
+++ src/control/levels/PunchLevel.java
@@ -11,25 +11,15 @@ import model.entities.HostileEntity;
 
 public abstract class PunchLevel extends Level
 {
-	private float proportion;
-	private float minThreshold;
-	private int lastSpawnedHostile = 0;
-	private int lastSpawned = 0;
-
 	public PunchLevel(Game game)
 	{
 		super(game);
-		if(getGame().getSong() != null){
-			minThreshold = getGame().getSong().getMinThreshold();
-			proportion = getGame().getSong().getMaxThreshold() - minThreshold;
-		}
 	}
 
 	@Override
 	public void update(double time)
 	{
 		super.update(time);
-		spawn(time);
 		
 		List<Entity> entities = getGame().getEntities();
 		Iterator<Entity> it = entities.iterator();
@@ -69,41 +59,6 @@ public abstract class PunchLevel extends Level
 						it.remove();
 					}
 				}
-			}
-		}
-	}
-
-	public abstract Entity getRandomEntity();
-
-	public abstract HostileEntity getRandomHostileEntity();
-
-	public void spawn(double time)
-	{
-		if(getGame().getSong() != null){
-			float current = (getGame().getSong().getThreshold() - minThreshold)
-					/ proportion;
-	
-			if (current != 0)
-			{
-				if (lastSpawnedHostile > (100 / (current * current * current)))
-				{
-					getGame().getEntities().add(getRandomHostileEntity());
-					lastSpawnedHostile = 0;
-				} else
-					lastSpawnedHostile += time;
-				
-				if(lastSpawned > 100 / current)
-				{
-					getGame().getEntities().add(getRandomEntity());
-					lastSpawned = 0;
-				} else
-					lastSpawned += time;
-			}
-		}
-		else{
-			if((int)(Math.random() * (time * 1.0)) == 1){
-				getGame().getEntities().add(getRandomEntity());
-				getGame().getEntities().add(getRandomHostileEntity());
 			}
 		}
 	}
