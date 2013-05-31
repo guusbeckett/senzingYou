a4ff3e86febf6fdfd46f20b6e3a652cd40d9b340
Jan Willem Raats <jw.raats@gmail.com>
2013-05-31 11:53:49 +0200

    Made score a bit better

--- src/view/BeamerPanel.java
+++ src/view/BeamerPanel.java
@@ -1,17 +1,26 @@
 package view;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
 import model.Camera;
 import model.Game;
+import model.User;
 import model.levels.Level;
 
 public class BeamerPanel extends JPanel implements ActionListener
@@ -26,7 +35,30 @@ public class BeamerPanel extends JPanel implements ActionListener
 		setBackground(Color.BLACK);
 		new Timer(1000 / 30, this).start();
 	}
-
+	
+	private void drawText(Graphics2D g2, String text, Color color, int size, Point2D p2)
+	{
+		AffineTransform transform = new AffineTransform();
+		transform.translate(p2.getX(), p2.getY());
+		drawText(g2, text, color, size, transform);
+	}
+	
+	private void drawText(Graphics2D g2, String text, Color color, int size, AffineTransform transform)
+	{
+		Font font = new Font("Arial", Font.BOLD, size);
+		FontRenderContext frc = g2.getFontRenderContext();
+		GlyphVector gv = font.createGlyphVector(frc, text);
+		Shape outline = gv.getOutline(0, 0);
+		if(transform != null){
+			outline = transform.createTransformedShape(outline);
+		}
+		g2.setColor(color);
+		g2.fill(outline);
+		g2.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(2));
+		g2.draw(outline);
+	}
+	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
@@ -41,6 +73,23 @@ public class BeamerPanel extends JPanel implements ActionListener
 		if (level != null)
 		{
 			level.getGroundRenderer().draw(g2);
+		}
+		
+		if(!game.getCamera().getUsers().isEmpty()){
+			Color[] colors = new Color[]{Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.WHITE, Color.YELLOW, Color.LIGHT_GRAY};
+			ArrayList<User> copyUsers = new ArrayList<User>();
+			copyUsers.addAll(game.getCamera().getUsers());
+			Collections.sort(copyUsers);
+			int x = 300;
+			int scoreWidth = (game.getCamera().VIEW_WIDTH - x) / copyUsers.size();
+	
+			for(User u: copyUsers)
+			{
+				if (u.isVisible()){
+					drawText(g2, ""+u.getScore(), colors[(u.getId() - 1)%colors.length], 25, new Point2D.Double(x, 25));
+				}
+				x+=scoreWidth;
+			}
 		}
 	}
 
