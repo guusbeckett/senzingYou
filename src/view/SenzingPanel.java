361054299f82af965ca9b37640861a8abd7b1d74
imp <johannes.san@gmail.com>
2013-06-05 13:15:06 +0200

    added translate of mirrored entities

--- src/view/SenzingPanel.java
+++ src/view/SenzingPanel.java
@@ -33,35 +33,37 @@ import model.levels.Level;
 
 public class SenzingPanel extends JPanel implements ActionListener
 {
-	private static final  long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	private Game game;
 	private Image rewardImage, losingImage;
-	
+
 	public SenzingPanel(Game game)
 	{
 		super();
 		this.game = game;
 		setBackground(Color.BLACK);
-		new Timer(1000/30, this).start();
+		new Timer(1000 / 30, this).start();
 		rewardImage = MediaProvider.getInstance().getImage("reward.png");
 		losingImage = MediaProvider.getInstance().getImage("losingpoint.png");
 	}
-	
-	
-	private void drawText(Graphics2D g2, String text, Color color, int size, Point2D p2)
+
+	private void drawText(Graphics2D g2, String text, Color color, int size,
+			Point2D p2)
 	{
 		AffineTransform transform = new AffineTransform();
 		transform.translate(p2.getX(), p2.getY());
 		drawText(g2, text, color, size, transform);
 	};
-	
-	private void drawText(Graphics2D g2, String text, Color color, int size, AffineTransform transform)
+
+	private void drawText(Graphics2D g2, String text, Color color, int size,
+			AffineTransform transform)
 	{
 		Font font = new Font("Arial", Font.BOLD, size);
 		FontRenderContext frc = g2.getFontRenderContext();
 		GlyphVector gv = font.createGlyphVector(frc, text);
 		Shape outline = gv.getOutline(0, 0);
-		if(transform != null){
+		if (transform != null)
+		{
 			outline = transform.createTransformedShape(outline);
 		}
 		g2.setColor(color);
@@ -70,50 +72,67 @@ public class SenzingPanel extends JPanel implements ActionListener
 		g2.setStroke(new BasicStroke(2));
 		g2.draw(outline);
 	}
-	
+
 	private void drawEntities(Graphics2D g2, int step)
 	{
 		for (Entity entity : game.getEntities())
 		{
-			AffineTransform ax = new AffineTransform();			
-			
-			ax.rotate(entity.getRotation(), entity.getRotationPoint().getX() + entity.getPosition().getX(), entity.getRotationPoint().getY() + entity.getPosition().getY());
-			
+			AffineTransform ax = new AffineTransform();
+
+			ax.rotate(entity.getRotation(), entity.getRotationPoint().getX()
+					+ entity.getPosition().getX(), entity.getRotationPoint()
+					.getY() + entity.getPosition().getY());
+
 			if (entity.getImage() != null)
 			{
-				if((step == 0 && !(entity instanceof HostileEntity)) ||
-						(step == 1 && (entity instanceof HostileEntity) && ((HostileEntity) entity).isAlive()))
+				if ((step == 0 && !(entity instanceof HostileEntity))
+						|| (step == 1 && (entity instanceof HostileEntity) && ((HostileEntity) entity)
+								.isAlive()))
 				{
-					ax.translate(entity.getPosition().getX(), entity.getPosition().getY());
-					ax.scale(entity.getDimensions().getWidth() / entity.getImage().getWidth(null) * ((entity.isMirrored()) ? -1 : 1), entity.getDimensions().getHeight() / entity.getImage().getHeight(null));
+					ax.translate(entity.getPosition().getX(), entity
+							.getPosition().getY());
+					ax.scale(
+							entity.getDimensions().getWidth()
+									/ entity.getImage().getWidth(null)
+									* ((entity.isMirrored()) ? -1 : 1), entity
+									.getDimensions().getHeight()
+									/ entity.getImage().getHeight(null));
+					if (entity.isMirrored())
+						ax.translate(entity.getDimensions().getWidth(), 0);
 					g2.drawImage(entity.getImage(), ax, null);
 				}
-				
+
 				else if (step == 1 && entity instanceof HostileEntity)
 				{
 					HostileEntity hostile = (HostileEntity) entity;
-					
+
 					if (!hostile.isAlive())
 					{
-						Image image = (hostile.getReward() <= 0) ? losingImage : rewardImage;
-						
-						ax.translate(hostile.getDeadLocation().getX(), hostile.getDeadLocation().getY());
-						ax.scale(entity.getDimensions().getWidth() / image.getWidth(null), entity.getDimensions().getHeight() / image.getHeight(null));
+						Image image = (hostile.getReward() <= 0) ? losingImage
+								: rewardImage;
+
+						ax.translate(hostile.getDeadLocation().getX(), hostile
+								.getDeadLocation().getY());
+						ax.scale(
+								entity.getDimensions().getWidth()
+										/ image.getWidth(null),
+								entity.getDimensions().getHeight()
+										/ image.getHeight(null));
 						g2.drawImage(image, ax, null);
-						
+
 						ax.translate(0, (image.getWidth(null) / 1.5));
-						drawText(g2, ""+hostile.getReward(), Color.BLUE, 250, ax);
+						drawText(g2, "" + hostile.getReward(), Color.BLUE, 250,
+								ax);
 					}
 				}
 			}
 		}
 	}
-	
-	
+
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D)g;
+		Graphics2D g2 = (Graphics2D) g;
 
 		double _w = getWidth();
 		double _h = getHeight();
@@ -121,75 +140,82 @@ public class SenzingPanel extends JPanel implements ActionListener
 		double _y = Camera.VIEW_HEIGHT;
 		double _s = _h / _y;
 		double _b = (_w / _s - _x) / 2;
-		
+
 		g2.scale(_s, _s);
 		g2.translate(_b, 0);
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		BufferedImage[] images = game.getCamera().getImageBackgroundAndForeground();
-		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		BufferedImage[] images = game.getCamera()
+				.getImageBackgroundAndForeground();
+
 		// Draw background
 		Level level = game.getLevel();
-		
+
 		if (level != null)
 			g2.drawImage(level.getBackground(), 0, 0, null);
-		
+
 		// Draw camera background image
 		g2.drawImage(images[0], null, 0, 0);
-		
+
 		// Draw the background entities
 		drawEntities(g2, 0);
-		
+
 		// Draw camera foreground image
 		g2.drawImage(images[1], null, 0, 0);
-		
+
 		// Draw the foreground entities
 		drawEntities(g2, 1);
 
 		// Draw description of level
-		if(level != null)
+		if (level != null)
 		{
-			if(level.isDescriptionImageVisible())
+			if (level.isDescriptionImageVisible())
 			{
 				AffineTransform tr = new AffineTransform();
 				tr.scale(.7, .7);
 				g2.drawImage(level.getDescriptionImage(), tr, null);
 			}
 		}
-		
+
 		// Draw all the scores
-		if(!game.getCamera().getUsers().isEmpty())
+		if (!game.getCamera().getUsers().isEmpty())
 		{
-			Color[] colors = new Color[]{Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.WHITE, Color.YELLOW, Color.LIGHT_GRAY};
+			Color[] colors = new Color[] { Color.RED, Color.BLUE, Color.GREEN,
+					Color.ORANGE, Color.WHITE, Color.YELLOW, Color.LIGHT_GRAY };
 			ArrayList<User> copyUsers = new ArrayList<User>();
 			copyUsers.addAll(game.getCamera().getUsers());
 			Collections.sort(copyUsers);
 			int x = 300;
 			int scoreWidth = (Camera.VIEW_WIDTH - x) / copyUsers.size();
-	
-			for(User u: copyUsers)
+
+			for (User u : copyUsers)
 			{
-				if (u.isVisible()){
-					drawText(g2, ""+u.getScore(), colors[(u.getId() - 1)%colors.length], 25, new Point2D.Double(x, 25));
+				if (u.isVisible())
+				{
+					drawText(g2, "" + u.getScore(), colors[(u.getId() - 1)
+							% colors.length], 25, new Point2D.Double(x, 25));
 				}
-				x+=scoreWidth;
+				x += scoreWidth;
 			}
 		}
-		
+
 		// Draw the countdown
 		Song song = game.getSong();
-		
+
 		if (song != null)
 		{
-			int time = (int)song.getTime();
-			int length = (int)song.getLength();
-			drawText(g2, String.format("%02d:%02d / %02d:%02d", time / 60, time % 60, length / 60, length % 60), Color.ORANGE, 25, new Point2D.Double(8, 25));
+			int time = (int) song.getTime();
+			int length = (int) song.getLength();
+			drawText(g2, String.format("%02d:%02d / %02d:%02d", time / 60,
+					time % 60, length / 60, length % 60), Color.ORANGE, 25,
+					new Point2D.Double(8, 25));
 		}
-		
+
 		// Draw sideboxes
 		g2.setColor(Color.BLACK);
 		g2.fill(new Rectangle2D.Double(-_b, 0, _b, _y));
-		g2.fill(new Rectangle2D.Double(_x, 0, _b, _y));	
+		g2.fill(new Rectangle2D.Double(_x, 0, _b, _y));
 	}
 
 	@Override
