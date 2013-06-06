9233a699a67b3bd21992064758a2f72d6fec331c
imp <johannes.san@gmail.com>
2013-06-06 15:52:01 +0200

    try stuff

--- src/model/levels/underwater/UnderwaterGround.java
+++ src/model/levels/underwater/UnderwaterGround.java
@@ -16,7 +16,7 @@ public class UnderwaterGround implements GroundRenderer
 		AffineTransform tr = new AffineTransform();
 		tr.translate(-100, -100);
 		tr.scale(3, 3);
-		tr.shear(Math.sin(time / 900) / 2 + 0.5, Math.cos(time / 900) / 2 + 0.5);
+		tr.shear(Math.sin(time / 900) / 10, Math.cos(time / 900) / 10);
 		g2.drawImage(MediaProvider.getInstance().getImage("underwater/ground.jpg"), tr, null);
 	}
 
