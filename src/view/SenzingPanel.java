ca6b46b0bf8cbeba5b36aa0c99a3770069e24e38
Sven van Heugten <svenvanheugten@home.nl>
2013-06-06 11:47:28 +0200

    Added image that tells you to connect USB and centered other images.

--- src/view/SenzingPanel.java
+++ src/view/SenzingPanel.java
@@ -47,16 +47,14 @@ public class SenzingPanel extends JPanel implements ActionListener
 		losingImage = MediaProvider.getInstance().getImage("losingpoint.png");
 	}
 
-	private void drawText(Graphics2D g2, String text, Color color, int size,
-			Point2D p2)
+	private void drawText(Graphics2D g2, String text, Color color, int size, Point2D p2)
 	{
 		AffineTransform transform = new AffineTransform();
 		transform.translate(p2.getX(), p2.getY());
 		drawText(g2, text, color, size, transform);
-	};
+	}
 
-	private void drawText(Graphics2D g2, String text, Color color, int size,
-			AffineTransform transform)
+	private void drawText(Graphics2D g2, String text, Color color, int size, AffineTransform transform)
 	{
 		Font font = new Font("Arial", Font.BOLD, size);
 		FontRenderContext frc = g2.getFontRenderContext();
@@ -72,6 +70,19 @@ public class SenzingPanel extends JPanel implements ActionListener
 		g2.setStroke(new BasicStroke(2));
 		g2.draw(outline);
 	}
+	
+	private void drawImageInCenter(Graphics2D g2, Image image)
+	{
+		double width = image.getWidth(null);
+		double height = image.getHeight(null);
+
+		AffineTransform ax = new AffineTransform();
+		
+		ax.translate(Camera.VIEW_WIDTH / 2 - 320/2, Camera.VIEW_HEIGHT / 2 - 240/2);
+		ax.scale(320 / width, 240 / height);
+		
+		g2.drawImage(image, ax, null);
+	}
 
 	private void drawEntities(Graphics2D g2, int step)
 	{
@@ -79,26 +90,16 @@ public class SenzingPanel extends JPanel implements ActionListener
 		{
 			AffineTransform ax = new AffineTransform();
 
-			ax.rotate(entity.getRotation(), entity.getRotationPoint().getX()
-					+ entity.getPosition().getX(), entity.getRotationPoint()
-					.getY() + entity.getPosition().getY());
+			ax.rotate(entity.getRotation(), entity.getRotationPoint().getX() + entity.getPosition().getX(), entity.getRotationPoint().getY() + entity.getPosition().getY());
 
 			if (entity.getImage() != null)
 			{
-				if ((step == 0 && !(entity instanceof HostileEntity))
-						|| (step == 1 && (entity instanceof HostileEntity) && ((HostileEntity) entity)
-								.isAlive()))
+				if ((step == 0 && !(entity instanceof HostileEntity)) || (step == 1 && (entity instanceof HostileEntity) && ((HostileEntity) entity).isAlive()))
 				{
-					ax.translate(entity.getPosition().getX(), entity
-							.getPosition().getY());
+					ax.translate(entity.getPosition().getX(), entity.getPosition().getY());
 					if (entity.isMirrored())
 						ax.translate(entity.getDimensions().getWidth(), 0);
-					ax.scale(
-							entity.getDimensions().getWidth()
-									/ entity.getImage().getWidth(null)
-									* ((entity.isMirrored()) ? -1 : 1), entity
-									.getDimensions().getHeight()
-									/ entity.getImage().getHeight(null));
+					ax.scale(entity.getDimensions().getWidth() / entity.getImage().getWidth(null) * ((entity.isMirrored()) ? -1 : 1), entity.getDimensions().getHeight() / entity.getImage().getHeight(null));
 					g2.drawImage(entity.getImage(), ax, null);
 				}
 
@@ -108,21 +109,14 @@ public class SenzingPanel extends JPanel implements ActionListener
 
 					if (!hostile.isAlive())
 					{
-						Image image = (hostile.getReward() <= 0) ? losingImage
-								: rewardImage;
+						Image image = (hostile.getReward() <= 0) ? losingImage : rewardImage;
 
-						ax.translate(hostile.getDeadLocation().getX(), hostile
-								.getDeadLocation().getY());
-						ax.scale(
-								entity.getDimensions().getWidth()
-										/ image.getWidth(null),
-								entity.getDimensions().getHeight()
-										/ image.getHeight(null));
+						ax.translate(hostile.getDeadLocation().getX(), hostile.getDeadLocation().getY());
+						ax.scale(entity.getDimensions().getWidth() / image.getWidth(null), entity.getDimensions().getHeight() / image.getHeight(null));
 						g2.drawImage(image, ax, null);
 
 						ax.translate(0, (image.getWidth(null) / 1.5));
-						drawText(g2, "" + hostile.getReward(), Color.BLUE, 250,
-								ax);
+						drawText(g2, "" + hostile.getReward(), Color.BLUE, 250, ax);
 					}
 				}
 			}
@@ -143,17 +137,21 @@ public class SenzingPanel extends JPanel implements ActionListener
 
 		g2.scale(_s, _s);
 		g2.translate(_b, 0);
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		BufferedImage[] images = game.getCamera()
-				.getImageBackgroundAndForeground();
+		BufferedImage[] images = game.getCamera().getImageBackgroundAndForeground();
 
 		// Draw background
 		Level level = game.getLevel();
 
-		if (level != null)
+		if (level == null)
+		{
+			drawImageInCenter(g2, MediaProvider.getInstance().getImage("usbConnect.png"));
+		}
+		else
+		{
 			g2.drawImage(level.getBackground(), 0, 0, null);
+		}
 
 		// Draw camera background image
 		g2.drawImage(images[0], null, 0, 0);
@@ -163,26 +161,23 @@ public class SenzingPanel extends JPanel implements ActionListener
 
 		// Draw camera foreground image
 		g2.drawImage(images[1], null, 0, 0);
-
-		// Draw the foreground entities
-		drawEntities(g2, 1);
-
+		
 		// Draw description of level
 		if (level != null)
 		{
 			if (level.isDescriptionImageVisible())
 			{
-				AffineTransform tr = new AffineTransform();
-				tr.scale(.7, .7);
-				g2.drawImage(level.getDescriptionImage(), tr, null);
+				drawImageInCenter(g2, level.getDescriptionImage());
 			}
 		}
+
+		// Draw the foreground entities
+		drawEntities(g2, 1);
 
 		// Draw all the scores
 		if (!game.getCamera().getUsers().isEmpty())
 		{
-			Color[] colors = new Color[] { Color.RED, Color.BLUE, Color.GREEN,
-					Color.ORANGE, Color.WHITE, Color.YELLOW, Color.LIGHT_GRAY };
+			Color[] colors = new Color[] { Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.WHITE, Color.YELLOW, Color.LIGHT_GRAY };
 			ArrayList<User> copyUsers = new ArrayList<User>();
 			copyUsers.addAll(game.getCamera().getUsers());
 			Collections.sort(copyUsers);
@@ -193,8 +188,7 @@ public class SenzingPanel extends JPanel implements ActionListener
 			{
 				if (u.isVisible())
 				{
-					drawText(g2, "" + u.getScore(), colors[(u.getId() - 1)
-							% colors.length], 25, new Point2D.Double(x, 25));
+					drawText(g2, "" + u.getScore(), colors[(u.getId() - 1) % colors.length], 25, new Point2D.Double(x, 25));
 				}
 				x += scoreWidth;
 			}
@@ -207,9 +201,7 @@ public class SenzingPanel extends JPanel implements ActionListener
 		{
 			int time = (int) song.getTime();
 			int length = (int) song.getLength();
-			drawText(g2, String.format("%02d:%02d / %02d:%02d", time / 60,
-					time % 60, length / 60, length % 60), Color.ORANGE, 25,
-					new Point2D.Double(8, 25));
+			drawText(g2, String.format("%02d:%02d / %02d:%02d", time / 60, time % 60, length / 60, length % 60), Color.ORANGE, 25, new Point2D.Double(8, 25));
 		}
 
 		// Draw sideboxes
