8f67b2a4cbf7879f3e93f2456b7a2bdebf1f335f
imp <johannes.san@gmail.com>
2013-06-07 14:07:57 +0200

    fixed beamer backgrounds

--- src/model/levels/cave/CaveGround.java
+++ src/model/levels/cave/CaveGround.java
@@ -1,23 +1,25 @@
 package model.levels.cave;
 
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 
 import model.GroundRenderer;
 import model.MediaProvider;
-
 
 public class CaveGround implements GroundRenderer
 {
 	@Override
 	public void draw(Graphics2D g2)
 	{
-		g2.drawImage(MediaProvider.getInstance().getImage("cave/ground.jpg"), null, null);
+		AffineTransform tr = new AffineTransform();
+		tr.translate(-400, 0);
+		g2.drawImage(MediaProvider.getInstance().getImage("cave/ground.jpg"), tr, null);
 	}
 
 	@Override
 	public void update(double time)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 }
