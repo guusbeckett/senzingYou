8792d6ca599ed58a6c389c6556b8358231a674b8
Jan Willem Raats <jw.raats@gmail.com>
2013-06-07 15:18:22 +0200

    Hightscore a bit better

--- src/model/Highscore.java
+++ src/model/Highscore.java
@@ -12,25 +12,11 @@ import java.util.List;
 public class Highscore
 {
 	private List<Score> scores;
-	private String fileName;
+	private final String fileName = "saveFile.sav";
+	
 	public Highscore(){
-		fileName = "saveFile.sav";
-		
 		this.scores = new ArrayList<Score>();
-		//Reading the score
-		try
-		{
-			FileInputStream file = new FileInputStream(fileName);
-			ObjectInputStream restore = new ObjectInputStream(file);
-			
-			scores = (List<Score>) (restore.readObject());
-			restore.close();
-			file.close();
-			
-		} catch (Exception e)
-		{}
-		
-		
+		load();
 	}
 	
 	public List<Score> getScores(){
@@ -42,21 +28,37 @@ public class Highscore
 		Collections.sort(scores);
 		
 		//Saving the file
-	    try
+		save();
+	   
+	}
+	
+	public void load(){
+		//Reading the score
+		try
 		{
-	    	FileOutputStream file = new FileOutputStream(fileName);
-			ObjectOutputStream oos = new ObjectOutputStream(file);
-			oos.writeObject(scores);
-			oos.flush();
-			oos.close();
+			FileInputStream file = new FileInputStream(fileName);
+			ObjectInputStream restore = new ObjectInputStream(file);
+					
+			scores = (List<Score>) (restore.readObject());
+			restore.close();
 			file.close();
-		} catch (Exception e)
+					
+		}catch (Exception e)
 		{}
-	    
 	}
 	
 	public void save(){
-		
+		 try
+			{
+		    	FileOutputStream file = new FileOutputStream(fileName);
+				ObjectOutputStream oos = new ObjectOutputStream(file);
+				oos.writeObject(scores);
+				oos.flush();
+				oos.close();
+				file.close();
+			} catch (Exception e)
+			{}
+		    
 	}
 }
 
