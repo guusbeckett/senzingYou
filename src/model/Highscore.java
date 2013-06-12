450c4d728192dc9c7cdae9933a685a1bc74b305e
imp <johannes.san@gmail.com>
2013-06-12 10:14:13 +0200

    import fixed

--- src/model/Highscore.java
+++ src/model/Highscore.java
@@ -2,7 +2,6 @@ package model;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
